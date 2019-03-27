7382f6de5371aa626b119d9bd3ca07a9faab7088
Simon Zweimüller <43468189+simonZweimueller@users.noreply.github.com>
2019-03-27 19:58:33 +0100

    Add files via upload

---
+++ src/Components/Question.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const Question = props => {
+  return (
+    <div className="Question">
+      <p>{props.title}</p>
+    </div>
+  );
+};
+
+export default Question;
---
+++ src/Components/UserInput.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class UserInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert(this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          UserInput:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default UserInput;
