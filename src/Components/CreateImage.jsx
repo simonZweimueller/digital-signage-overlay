ba7a2f72a383b80d597ce49a8a53dc0881ae7b9d
tealeon <43637536+tealeon@users.noreply.github.com>
2019-03-27 17:11:13 +0100

    Update CreateImage.jsx

--- src/Components/CreateImage.jsx
+++ src/Components/CreateImage.jsx
@@ -38,40 +38,6 @@ class CreateImage extends Component {
     ctx.fillText(test, 40, 40);
   }
 
-  sendImage() {
-    //TODO http-get
-    //https://nodejs.org/api/http.html#http_http_get_options_callback --IMPORTANT
-    //192.168.43.129/library
-    //https://xibo.org.uk/manual/api/#/library/libraryAdd
-
-    let url = "192.168.43.129/library/";
-    let method = "POST";
-    let shouldBeAsync = true;
-    let request = new XMLHttpRequest();
-
-    //https://stackoverflow.com/questions/34485420/how-do-you-put-an-image-file-in-a-json-object
-    /*let img = new Image();
-    img.onload = function() {
-      let testCanvas = document.getElementById("myCanvas");
-      let testImg = testCanvas.toDataURL("image/png");
-    };
-    img.src = location.pathname;
-    console.log(img.src);*/
-
-    //converts canvas to png
-    let testCanvas = document.getElementById("myCanvas");
-    let testImg = testCanvas.toDataURL("image/png");
-
-    request.onload = function() {
-      var status = request.status;
-      var data = request.responseText;
-    };
-    request.open(method, url, shouldBeAsync);
-    request.setRequestHeader("Content-Type", "application/json;");
-    request.send(testImg);
-    console.log(testImg);
-  }
-
   render() {
     return (
       <React.Fragment>
@@ -96,8 +62,6 @@ class CreateImage extends Component {
           <br />
           <input type="submit" value="Create Image" />
         </form>
-        <button onClick={this.sendImage}>Send</button>{" "}
-        {/*evt <form> verwenden*/}
         <canvas ref="canvas" width={1920} height={1080} id="myCanvas" />
       </React.Fragment>
     );
