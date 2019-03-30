f5b0ac49690dd2362c1cd00c8a42b1ae209c05c4
Simon Zweimüller <43468189+simonZweimueller@users.noreply.github.com>
2019-03-30 17:54:43 +0100

    Add files via upload
    added more options for customisation of the image

--- src/Components/CreateImage.jsx
+++ src/Components/CreateImage.jsx
@@ -5,7 +5,7 @@ import { Layer, Rect, Stage, Group } from "react-konva";
 class CreateImage extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: "", color: "red" };
+    this.state = { value: "", imgWidth: 1920, imgHeight: 1080 };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -29,13 +29,20 @@ class CreateImage extends Component {
     const ctx = this.refs.canvas.getContext("2d");
     let font = document.getElementById("font").value;
     let fontColor = document.getElementById("fontColor").value;
+    let fontSize = document.getElementById("fontSize").value;
+    let fontPWidth = document.getElementById("fontPWidth").value;
+    let fontPHeight = document.getElementById("fontPHeight").value;
+    let bgColor = document.getElementById("bgColor").value;
+    document.getElementById("myCanvas").style.backgroundColor = bgColor;
+
+    console.log(fontColor);
 
     ctx.clearRect(0, 0, 1920, 1080);
     //ctx.fillText("Hello World", 20, 20);
-    ctx.font = "15px " + font;
+    ctx.font = fontSize + "px " + font;
     ctx.fillStyle = fontColor;
     ctx.textAlign = "center";
-    ctx.fillText(test, 40, 40);
+    ctx.fillText(test, fontPWidth, fontPHeight);
   }
 
   render() {
@@ -58,11 +65,30 @@ class CreateImage extends Component {
               <option>Calibri</option>
             </select>
           </label>
+          <br />
+          <label>Font Color: </label>
           <input id="fontColor" type="color" />
+          <br />
+          <label>Font Size (in px): </label>
+          <input id="fontSize" type="number" />
+          <br />
+          <label>Font Position width (in px): </label>
+          <input id="fontPWidth" type="number" />
+          <br />
+          <label>Font Position height (in px): </label>
+          <input id="fontPHeight" type="number" />
+          <br />
+          <label>Background Color: </label>
+          <input id="bgColor" type="color" />
           <br />
           <input type="submit" value="Create Image" />
         </form>
-        <canvas ref="canvas" width={1920} height={1080} id="myCanvas" />
+        <canvas
+          ref="canvas"
+          width={this.state.imgWidth}
+          height={this.state.imgHeight}
+          id="myCanvas"
+        />
       </React.Fragment>
     );
   }
